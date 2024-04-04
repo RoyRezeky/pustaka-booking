bc5d7f0f23cd678873e539729493d4d31404ce93
RoyRezeky <royrezsim@gmail.com>
2024-04-04 20:48:46 +0700

    menambahkan v_about.php,v_footer.php,v_header.php,v_index.php,web.php

---
+++ application/views/v_about.php
@@ -0,0 +1,26 @@
+<section>
+<link rel="stylesheet" type="text/css" 
+href="http://localhost/pustaka-booking/assets/css/stylebuku.css">
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan : Roy</li>
+        <li>Nama Belakang : Rezeky</li>
+    </ul>
+    <br>
+    <h4>Alamat</h4>
+    <ul type="none">
+        <li> Jalan Karang Satria</li>
+    </ul>
+ 
+    <h4>Tempat Lahir</h4>
+    <ul type="none">
+        <li>Bekasi</li>
+    </ul>
+ 
+    <h4>Olah Raga Favorit</h4>
+    <ul type="square">
+        <li>Bulutangkis</li>
+        <li>Berenang</li>
+    </ul>
+</section>
